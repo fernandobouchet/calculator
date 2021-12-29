f9e197a14a4638783baabfcdc3517a1e71fe61ae
fernandobouchet <fernandobouchet@gmail.com>
2021-12-29 20:18:15 -0300

    Added working basic calculator TODO: Code optimizations, some more functions and better css style

---
+++ main.js
@@ -0,0 +1,108 @@
+const displayUp = document.getElementById("display-up");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operators");
+const clearButton = document.getElementById("clear");
+const equalButton = document.getElementById("equal");
+let actualOperator = 0;
+let previousOperator = 0;
+let operation = undefined;
+
+function numberButtons() {
+  numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+      addNumber(number.value);
+    });
+  });
+}
+
+function operatorButtons() {
+  operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+      selectOperation(operator.value);
+    });
+  });
+}
+
+function showResult() {
+  equalButton.addEventListener("click", () => {
+    calculate();
+    updateDisplay();
+  });
+}
+
+function clearDisplay() {
+  clearButton.addEventListener("click", () => {
+    clear();
+    updateDisplay();
+  });
+}
+
+function addNumber(number) {
+  actualOperator = actualOperator.toString() + number.toString();
+  updateDisplay();
+}
+
+function selectOperation(operatorValue) {
+  if (actualOperator === "") return;
+  if (previousOperator !== "") {
+    calculate();
+  }
+  operation = operatorValue.toString();
+  previousOperator = actualOperator;
+  actualOperator = "";
+}
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function calculate() {
+  let result = 0;
+  const firstNumber = parseFloat(actualOperator);
+  const secondNumber = parseFloat(previousOperator);
+  if (operation === "+") {
+    result = add(firstNumber, secondNumber);
+  } else if (operation === "-") {
+    result = subtract(firstNumber, secondNumber);
+  } else if (operation === "*") {
+    result = multiply(firstNumber, secondNumber);
+  } else if (operation === "/") {
+    result = divide(firstNumber, secondNumber);
+  }
+  actualOperator = result;
+  operation = undefined;
+  previousOperator = "";
+}
+
+function updateDisplay() {
+  displayUp.textContent = actualOperator;
+}
+
+function clear() {
+  actualOperator = 0;
+  previousOperator = 0;
+  operation = undefined;
+}
+
+function start() {
+  clear();
+  numberButtons();
+  operatorButtons();
+  showResult();
+  clearDisplay();
+  calculate();
+}
+
+start();
